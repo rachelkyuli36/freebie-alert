ab6d8f75af78dc6a3e3747f967ddaba6342ae351
rsl2159 Lee <rsl2159@columbia.edu>
2022-10-28 01:25:12 +0000

    change seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@
 # title, event_date, release_date, date_posted_type
 
 posts = [
-  { title: 'Waterbottles on Lerner Ramps', event_date: '2020-10-20', date_posted: '2020-10-19', type: 'merch'}
+  { title: 'Waterbottles on Lerner Ramps', rating: 'Merch', event_date: '2020-10-20', date_posted: '2020-10-19'},
+  { title: 'Mochi Mingle', rating: 'Food', event_date: '2022-11-20', date_posted: '2022-10-27'},
+  { title: 'Pojangmacha', rating: 'Food', event_date: '2022-11-03', date_posted: '2022-10-27'},
 ]
 
 posts.each do |post|
-  Post.create!(post)
+  Movie.create!(post)
 end
