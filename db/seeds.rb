371b50284134c5ddddbb1cd649fead0d6ef75011
rkc2134 Rachel Chung <rkc2134@columbia.edu>
2022-10-28 01:03:42 +0000

    add example seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = [
-  { title: 'Aladdin', rating: 'G', release_date: '25-Nov-1992' },
-  { title: 'The Terminator', rating: 'R', release_date: '26-Oct-1984' },
-  { title: 'When Harry Met Sally', rating: 'R', release_date: '21-Jul-1989' },
-  { title: 'The Help', rating: 'PG-13', release_date: '10-Aug-2011' },
-  { title: 'Chocolat', rating: 'PG-13', release_date: '5-Jan-2001' },
-  { title: 'Amelie', rating: 'R', release_date: '25-Apr-2001' },
-  { title: '2001: A Space Odyssey', rating: 'G', release_date: '6-Apr-1968' },
-  { title: 'The Incredibles', rating: 'PG', release_date: '5-Nov-2004' },
-  { title: 'Raiders of the Lost Ark', rating: 'PG', release_date: '12-Jun-1981' },
-  { title: 'Chicken Run', rating: 'G', release_date: '21-Jun-2000' },
+# title, event_date, release_date, date_posted_type
+
+posts = [
+  { title: 'Waterbottles on Lerner Ramps', event_date: '2020-10-20', date_posted: '2020-10-19', type: 'merch'}
 ]
 
-movies.each do |movie|
-  Movie.create!(movie)
+posts.each do |post|
+  Post.create!(post)
 end
