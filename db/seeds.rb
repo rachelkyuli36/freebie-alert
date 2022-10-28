f195831d37dd91f32b9f495566e91403645ae6a7
rsl2159 Lee <rsl2159@columbia.edu>
2022-10-28 01:07:00 +0000

    seeds merge conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-<<<<<<< HEAD
-=======
 # title, event_date, release_date, date_posted_type
 
 posts = [
   { title: 'Waterbottles on Lerner Ramps', event_date: '2020-10-20', date_posted: '2020-10-19', type: 'merch'}
->>>>>>> 371b50284134c5ddddbb1cd649fead0d6ef75011
 ]
 
 posts.each do |post|
