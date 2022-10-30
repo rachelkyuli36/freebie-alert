41bcc6897dda66ee6d63b644d9ac4e62748148e1
rkc2134 Rachel Chung <rkc2134@columbia.edu>
2022-10-30 19:17:47 +0000

    created spec starter files

--- db/migrate/20111119180638_create_movies.rb
+++ db/migrate/20111119180638_create_movies.rb
@@ -1,13 +1,13 @@
 class CreateMovies < ActiveRecord::Migration
   def up
     create_table :movies do |t|
-      t.string :title
-      t.string :rating
-      t.text :description
-      t.datetime :release_date
-      # Add fields that let Rails automatically keep track
-      # of when movies are added or modified:
-      t.timestamps
+    t.string :title
+    t.string :rating
+    t.text :description
+    t.datetime :release_date
+    # Add fields that let Rails automatically keep track
+    # of when movies are added or modified:
+    t.timestamps
     end
   end
 
